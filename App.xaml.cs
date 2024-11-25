0d505df704d7582efaa95286ab1c1d5c221d4b51
IULIA ȘUT <iulia.sut@stud.ubbcluj.ro>
2024-11-25 15:07:19 +0200

    Modificarile efectuate pentru Laborator8.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,28 @@
-﻿namespace SutIuliaLab7
+﻿using System;
+using SutIuliaLab7.Data;
+using System.IO;
+
+namespace SutIuliaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
     }
